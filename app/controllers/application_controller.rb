47afe6fccd2f3aa39984b772946718ea523d6c11
claudia v <claudia4ro@yahoo.com>
2020-06-21 20:07:12 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < Sinatra::Base
   
   post '/recipes' do
     @recipe= Recipe.create(params)
-    redirect "/recipes/#{@recipe.last.id}"
+    redirect "/recipes/#{@recipe.id}"
   end 
   
   
